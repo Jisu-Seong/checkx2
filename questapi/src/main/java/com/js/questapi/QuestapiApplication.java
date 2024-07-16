92556b3d0fb29d29a4efa53db7a80eb8435ac4da
Jisu-Seong <sjangela0615@gmail.com>
2024-07-16 16:33:57 +0900

    asdf

---
+++ questapi/src/main/java/com/js/questapi/QuestapiApplication.java
@@ -0,0 +1,13 @@
+package com.js.questapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestapiApplication.class, args);
+	}
+
+}
