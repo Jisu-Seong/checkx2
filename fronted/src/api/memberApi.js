f1d745ad19e028588ff7bb03bef4db07d968bfa2
Jisu-Seong <sjangela0615@gmail.com>
2024-07-19 14:25:18 +0900

    gdfsdfasdf

--- fronted/src/api/memberApi.js
+++ fronted/src/api/memberApi.js
@@ -6,7 +6,7 @@ export const loginPost = async (loginParam) => {
     const header = {headers: { "Content-Type": "x-www-form-urlencoded"}};
 
     const form = new FormData();
-    form.append("email", loginParam.email);
+    form.append("username", loginParam.email);
     form.append("password", loginParam.pw);
 
     const res = await axios.post(`${host}/login`, form, header);
